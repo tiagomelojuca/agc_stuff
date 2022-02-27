32f057c85e871a82d19103d6b6dfb98bb69738ce
Tiago Melo Juca <contact@tiagomelojuca.com>
2022-02-27 08:36:38 -0300

    ref

--- agc.hpp
+++ agc.hpp
@@ -166,6 +166,46 @@ public:
     {
         return cols;
     }
+    bool is_same_size(const matrix& other) const
+    {
+        return rows == other.rows && cols == other.cols;
+    }
+    bool is_row_matrix() const
+    {
+        return rows == 1;
+    }
+    bool is_column_matrix() const
+    {
+        return cols == 1;
+    }
+    bool is_square_matrix() const
+    {
+        return rows == cols;
+    }
+    bool is_empty() const
+    {
+        return rows == 0 || cols == 0;
+    }
+    bool is_null() const
+    {
+        if (is_empty())
+        {
+            return false;
+        }
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= cols; j++)
+            {
+                if ((*this)[i][j] != 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
     void transform(int row_begin, int row_end, int col_begin, int col_end,
                    std::function<T(T)> unary_op)
@@ -182,9 +222,9 @@ public:
     void transform(int row_begin, int row_end, int col_begin, int col_end,
                    const matrix& other, std::function<T(T, T)> binary_op)
     {
-        if (rows != other.rows || cols != other.cols)
+        if (!is_same_size(other))
         {
-            throw std::domain_error("matrix other must have same size");
+            throw std::domain_error("matrix transform must have same size");
         }
 
         for (int i = row_begin; i <= row_end; i++)
@@ -259,7 +299,7 @@ public:
     }
     bool operator==(const matrix& other) const
     {
-        if (rows != other.rows || cols != other.cols) {
+        if (!is_same_size(other)) {
             return false;
         }
 
@@ -308,7 +348,7 @@ public:
     }
     matrix& operator+=(const matrix& other)
     {
-        if (rows != other.rows || cols != other.cols)
+        if (!is_same_size(other))
         {
             throw std::domain_error("matrix addition must have same size");
         }
@@ -319,7 +359,7 @@ public:
     }
     matrix& operator-=(const matrix& other)
     {
-        if (rows != other.rows || cols != other.cols)
+        if (!is_same_size(other))
         {
             throw std::domain_error("matrix subtraction must have same size");
         }
@@ -335,7 +375,10 @@ public:
     }
     matrix& operator/=(T value)
     {
-        if (value == 0) { throw std::domain_error("divison by zero not allowed"); }
+        if (value == 0) {
+            throw std::domain_error("matrix divison by zero is not allowed");
+        }
+
         transform([&value](T a_ij) -> T { return a_ij / value; });
         return *this;
     }
@@ -344,7 +387,7 @@ public:
         if (cols != other.rows)
         {
             throw std::domain_error(
-                "matrix multiplication requires nC[A] == nR[B]"
+                "matrix multiplication requires numColumns(A) == numRows(B)"
             );
         }
 
@@ -364,11 +407,9 @@ public:
     }
     matrix times(const matrix& other) const
     {
-        if (rows != other.rows || cols != other.cols)
+        if (!is_same_size(other))
         {
-            throw std::domain_error(
-                "matrix times requires nC[A] == nC[B] and nR[A] = nR[B]"
-            );
+            throw std::domain_error("matrix times must have same size");
         }
 
         matrix<T> P(rows, cols);
@@ -384,9 +425,9 @@ public:
     }
     matrix pow(int n) const
     {
-        if (cols != rows)
+        if (!is_square_matrix())
         {
-            throw std::domain_error("matrix pow requires nC[A] == nR[A]");
+            throw std::domain_error("matrix pow requires a square matrix");
         }
         if (n < 1)
         {
